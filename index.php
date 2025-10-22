ee44182714bbbc27aa76bfc21ad6ae18eb9c5880
DilanMC22 <dilanmenachaves@gmail.com>
2025-10-22 08:49:08 -0600

    gg
    h

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+<<<<<<< Updated upstream
   header("Location: View/Inicio/IniciarSesion.php");
   exit;
 ?>
+=======
+  header("location: View/Inicio/IniciarSesion.php");
+  exit;
+?>
+>>>>>>> Stashed changes
